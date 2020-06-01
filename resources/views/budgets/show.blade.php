862b2ef69dbfa80698fb16c907637381d6aa09da
karwackid <damian.karwacki@slaskie.pl>
2020-06-01 20:28:23 +0200

    budget

---
+++ resources/views/budgets/show.blade.php
@@ -0,0 +1,51 @@
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card text-center">
+                    <div class="card-header">
+                        <ul class="nav nav-tabs card-header-tabs">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="{{ url('budget/1') }}">Styczeń</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="{{ url('budget/2') }}">Luty</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="{{ url('budget/3') }}">Marzec</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="{{ url('budget/4') }}">Kwiecień</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="{{ url('budget/5') }}">Maj</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="{{ url('budget/6') }}">Czerwiec</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="{{ url('budget/7') }}">Lipiec</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="{{ url('budget/8') }}">Sierpień</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="{{ url('budget/9') }}">Wrzesień</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="{{ url('budget/10') }}">Październik</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="{{ url('budget/11') }}">Listopad</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="{{ url('budget/12') }}">Grudzień</a>
+                            </li>
+                        </ul>
+                    </div>
+                    @include('budgets.single')
+                </div>
+            </div>
+        </div>
+    </div>
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,15 +25,15 @@
     <!-- MDB icon -->
     <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+    <link rel="stylesheet" href="{{ asset('https://use.fontawesome.com/releases/v5.11.2/css/all.css') }}">
     <!-- Google Fonts Roboto -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <link rel="stylesheet" href="{{ asset('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap') }}">
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset ('css/bootstrap.min.css') }}">
     <!-- Material Design Bootstrap -->
-    <link rel="stylesheet" href="css/mdb.min.css">
+    <link rel="stylesheet" href="{{ asset('css/mdb.min.css') }}">
     <!-- Your custom styles (optional) -->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
     <div id="app">
@@ -71,6 +71,13 @@
                             @if(Auth::check())
                                 <div class="form-group">
                                     <a  class="nav-link" href="{{ url('/wall') }}">Tablica </a>
+                                </div>
+                            @endif
+                        </li>
+                        <li class="nav-item">
+                            @if(Auth::check())
+                                <div class="form-group">
+                                    <a  class="nav-link" href="{{ url('/budget') }}">Budżet </a>
                                 </div>
                             @endif
                         </li>
